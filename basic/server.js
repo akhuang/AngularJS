3a870b070c6d95b04ee65a67084578b8922842d9
phoenix <fy24h.huang@gmail.com>
2016-09-12 06:49:14 +0800

    add fountain generator

--- basic/server.js
+++ basic/server.js
@@ -5,7 +5,7 @@ var express = require('express'),
 app.use(express.static(__dirname + "/public"));
 
 app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(8081);
